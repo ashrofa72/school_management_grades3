3d9768f13df7fd8f9357facffd88bd568a7006e5
Ashraf <as.ka1@hotmail.com>
2025-01-04 22:26:19 +0200

    28 commit

--- pages/students/first-year/page.js
+++ pages/students/first-year/page.js
@@ -1,72 +1,233 @@
-'use client'; // Add this at the very top of your file
+'use client';
+import ProtectedRoute from '../../../components/ProtectedRoute';
 import Head from 'next/head';
-import Link from 'next/link';
 import Navbar from '../../../components/navbar';
-import styles from '../../../styles/FirstYear.module.css';
-import { useState } from 'react';
+import styles from '../../../styles/SecondYear.module.css';
+import styles2 from '../../../styles/Table.module.css';
+import { useState, useEffect, useRef } from 'react';
 
-export default function FirstYear() {
-  const [selectedSubject, setSelectedSubject] = useState('Math');
+export default function SecondYear() {
+  const [selectedRoom, setSelectedRoom] = useState('2-1');
+  const [selectedSubject, setSelectedSubject] = useState('Biology');
+  const [selectedMonth, setSelectedMonth] = useState('October'); // State for month
+  const [dataRows, setDataRows] = useState([]);
+  const [error, setError] = useState(null);
+  const tableRef = useRef(); // Reference to the table for printing
 
-  const handleSelectChange = (event) => {
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        console.log(
+          `Fetching data for Room: ${selectedRoom}, Subject: ${selectedSubject}, Month: ${selectedMonth}`
+        );
+        const response = await fetch(
+          `/api/sheets?Room=${selectedRoom}&Subject=${selectedSubject}&Month=${selectedMonth}`
+        );
+
+        if (!response.ok) {
+          throw new Error(
+            `Network response was not ok: ${response.status} - ${response.statusText}`
+          );
+        }
+
+        const data = await response.json();
+        console.log('Fetched Data:', data);
+        setDataRows(data);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+        setError('Failed to fetch data. Please try again.');
+      }
+    }
+
+    fetchData();
+  }, [selectedRoom, selectedSubject, selectedMonth]);
+
+  const handleRoomChange = (event) => {
+    setSelectedRoom(event.target.value);
+  };
+
+  const handleSubjectChange = (event) => {
     setSelectedSubject(event.target.value);
   };
 
+  const handleMonthChange = (event) => {
+    setSelectedMonth(event.target.value);
+  };
+
+  const handlePrint = () => {
+    const printContent = tableRef.current.innerHTML;
+    const printWindow = window.open('', '_blank');
+    printWindow.document.write(`
+      <html>
+        <head>
+          <title>${selectedSubject} كشف درجات</title>
+          <style>
+            /* Custom print styles */
+            @import url('https://fonts.googleapis.com/css2?family=Almarai:wght@700&family=Marhey:wght@300..700&display=swap');
+            body {
+              font-family: Marhey, sans-serif;
+            }
+            h1 {
+              direction: rtl;
+              font-size: 15px;
+              text-align: right;
+            }
+            table {
+              border-collapse: collapse;
+              width: 100%;
+              direction: rtl;
+              font-family: Marhey, sans-serif;
+              font-size: 10px;
+            }
+            th, td {
+              border: 1px solid black;
+              padding: 8px;
+              text-align: center;
+            }
+          </style>
+        </head>
+        <body>
+          <h1>المادة: ${selectedSubject} | الشهر: ${selectedMonth}</h1>
+          ${printContent}
+        </body>
+      </html>
+    `);
+    printWindow.document.close();
+    printWindow.print();
+  };
+
   return (
-    <div>
-      <Navbar />
-      <div className={styles.container}>
-        <Head>
-          <title>طلاب الصف الأول</title>
-          <meta
-            name="description"
-            content="Information about first year students"
-          />
-          <link rel="icon" href="/favicon.ico" />
-        </Head>
+    <ProtectedRoute>
+      <div>
+        <Navbar />
+        <div className={styles.container}>
+          <Head>
+            <title>طلاب الصف الثاني</title>
+            <meta
+              name="description"
+              content="Information about second year students"
+            />
+            <link rel="icon" href="/favicon.ico" />
+          </Head>
 
-        {/* Header Section */}
-        <section className={styles.header}>
-          <h1>طلاب الصف الأول</h1>
-        </section>
+          <section className={styles.header}>
+            <h1>طلاب الصف الأول</h1>
+          </section>
 
-        {/* Dropdown Menu */}
-        <section className={styles.dropdown}>
-          <label htmlFor="subjects">Choose a Class:</label>
-          <select
-            id="subjects"
-            value={selectedSubject}
-            onChange={handleSelectChange}
-          >
-            <option value="1-1">1-1</option>
-            <option value="1-2">1-2</option>
-            <option value="1-3">1-3</option>
-            <option value="1-4">1-4</option>
-            <option value="1-5">1-5</option>
-            <option value="1-6">1-6</option>
-            <option value="1-7">1-7</option>
-            <option value="1-8">1-8</option>
-          </select>
-        </section>
+          {/* Dropdown Section */}
+          <section className={styles.dropdown}>
+            <div>
+              <label htmlFor="rooms">اختار الصف :</label>
+              <select
+                id="rooms"
+                value={selectedRoom}
+                onChange={handleRoomChange}
+              >
+                <option value="1-1">1-1</option>
+                <option value="1-2">1-2</option>
+                <option value="1-3">1-3</option>
+                <option value="1-4">1-4</option>
+                <option value="1-5">1-5</option>
+                <option value="1-6">1-6</option>
+                <option value="1-7">1-7</option>
+                <option value="1-8">1-8</option>
+              </select>
+            </div>
+            <div>
+              <label htmlFor="subjects">اختار المادة:</label>
+              <select
+                id="subjects"
+                value={selectedSubject}
+                onChange={handleSubjectChange}
+              >
+                <option value="Biology">Biology</option>
+                <option value="Maths 1 Science">Maths 1 Science</option>
+                <option value="Arabic">Arabic</option>
+                <option value="English First Language">
+                  English First Language
+                </option>
+                <option value="Maths 1 Arts">Maths 1 Arts</option>
+                <option value="Maths 2">Maths 2</option>
+                <option value="Chemistry">Chemistry</option>
+                <option value="French Second Language">
+                  French Second Language
+                </option>
+                <option value="Geography">Geography</option>
+                <option value="German Second Language">
+                  German Second Language
+                </option>
+                <option value="History">History</option>
+                <option value="Physics">Physics</option>
+                <option value="Psychology">Psychology</option>
+                <option value="Spanish Second Language">
+                  Spanish Second Language
+                </option>
+              </select>
+            </div>
+            <div>
+              <label htmlFor="months">اختار الشهر:</label>
+              <select
+                id="months"
+                value={selectedMonth}
+                onChange={handleMonthChange}
+              >
+                <option value="October">October</option>
+                <option value="November">November</option>
+                <option value="Term 1">Term 1</option>
+              </select>
+            </div>
+          </section>
 
-        {/* Content Section Based on Dropdown Selection */}
-        <section className={styles.content}>
-          {selectedSubject === '1-1' && <p>Content for 1-1...</p>}
-          {selectedSubject === '1-2' && <p>Content for 1-2...</p>}
-          {selectedSubject === '1-3' && <p>Content for 1-3...</p>}
-          {selectedSubject === '1-4' && <p>Content for 1-4...</p>}
-          {selectedSubject === '1-5' && <p>Content for 1-5...</p>}
-          {selectedSubject === '1-6' && <p>Content for 1-6...</p>}
-          {selectedSubject === '1-7' && <p>Content for 1-7...</p>}
-          {selectedSubject === '1-8' && <p>Content for 1-8...</p>}
-        </section>
+          {/* Data Table Section */}
+          <section className={styles.content}>
+            {error && <p>{error}</p>}
+            {dataRows.length > 0 ? (
+              <div>
+                <button
+                  className={`${styles2.printButton}`}
+                  onClick={handlePrint}
+                >
+                  طباعة الجدول
+                </button>
+                <div ref={tableRef}>
+                  <table className={styles2.table}>
+                    <thead>
+                      <tr>
+                        <th>اسم الطالبة</th>
+                        <th>الصف</th>
+                        <th>المادة</th>
+                        <th>كود الطالب</th>
+                        <th>التاريخ</th>
+                        <th>الدرجة</th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                      {dataRows.map((row, index) => (
+                        <tr key={index}>
+                          <td>{row.FullName}</td>
+                          <td>{row.Room}</td>
+                          <td>{row.Subject}</td>
+                          <td>{row.SISUserID}</td>
+                          <td>{row.StartDate}</td>
+                          <td>{row.Total}</td>
+                        </tr>
+                      ))}
+                    </tbody>
+                  </table>
+                </div>
+              </div>
+            ) : (
+              !error && <p>لا يوجد سجلات موجودة للصف والمادة والشهر المحددين</p>
+            )}
+          </section>
 
-        {/* Footer Section */}
-        <footer className={styles.footer}>
-          <p>&copy; 2024 مدرسة فاطمة الزهراء الثانوية للبنات</p>
-          <p>Developed by Ashraf Eltayb</p>
-        </footer>
+          {/* Footer Section */}
+          <footer className={styles.footer}>
+            <p>&copy; 2024 مدرسة فاطمة الزهراء الثانوية للبنات</p>
+            <p>Developed by Ashraf Eltayb</p>
+          </footer>
+        </div>
       </div>
-    </div>
+    </ProtectedRoute>
   );
 }
